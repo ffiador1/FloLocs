bd45fbf53899d8a9bb82f3ad5ec0d02596ea252a
Florina <ffiador@gmail.com>
2023-06-06 17:58:52 +0100

    security changes

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+export default function Home(){
+    return(<>
+        <div>
+            <Navbar/>
+        <img  src="imgs/BANNER,2.png"></img>
+            Header, book now button
+        </div>
+        <div>
+          working hours, contact us, schedule appointment  
+        </div>
+    </>);
+}
